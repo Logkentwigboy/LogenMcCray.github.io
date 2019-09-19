1e81c752aebe294b357924317cdd2422d84068e7
Logkentwigboy <twigboy16@byui.edu>
2019-09-19 16:46:19 -0700

    My Home Portal Page

---
+++ date.js
@@ -0,0 +1,30 @@
+let daynames=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday"];
+   
+   let months=[
+     "January",
+     "Feburary",
+     "March",
+     "April",
+     "May",
+     "June",
+     "July",
+     "August",
+     "September",
+     "October",
+     "November",
+     "December",
+   ]
+  
+   let d=new Date();
+   let dayname= daynames[d.getDay()];
+   let monthname= months [d.getMonth()];
+   let fulldate= dayname  +  ","  + monthname  +  "," +  d.getDay() + "," 
+  + d.getFullYear();
+  document.getElementById("currentdate").textContent = fulldate;
