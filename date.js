1a81a82ef4fbe3dbd03a415164f546f7a05f4900
Logkentwigboy <twigboy16@byui.edu>
2019-10-01 09:22:54 -0700

    updated date

--- date.js
+++ date.js
@@ -1,31 +1,15 @@
-let daynames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thurday",
-  "Friday",
-  "Saturday"
-];
+function lastModified() {
+  document.getElementById("lastModified").textContent = document.lastModified;
+}
+ 
+function getYear(){
+  var d = new Date;
+  var year = d.getFullYear();
+  document.getElementById("coprightYear").textContent = year;
+}
 
-let months = [
-  "January",
-  "Feburary",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-]
-
-let d = new Date();
-let dayname = daynames[d.getDay()];
-let monthname = months[d.getMonth()];
-let fulldate = dayname + "," + monthname + "," + d.getDay() + "," +
-  d.getFullYear();
-document.getElementById("currentdate").textContent = fulldate;
+function updateDates()
+{
+  lastModified();
+  getYear();
+}
