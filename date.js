389bc1677a702bc69824fa7e1bf13044eee25d00
Logkentwigboy <twigboy16@byui.edu>
2019-10-01 15:45:33 -0700

    ye

--- date.js
+++ date.js
@@ -2,10 +2,10 @@ function lastModified() {
   document.getElementById("lastModified").textContent = document.lastModified;
 }
  
-function getYear(){
+function getYear() {
   var d = new Date;
   var year = d.getFullYear();
-  document.getElementById("coprightYear").textContent = year;
+  document.getElementById("copyrightYear").textContent = year;
 }
 
 function updateDates()
