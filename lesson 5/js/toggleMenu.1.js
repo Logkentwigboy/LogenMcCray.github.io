71643a3323d1474b964572af26d2de0f7c6db9d1
Logkentwigboy <twigboy16@byui.edu>
2019-10-16 12:17:25 -0700

    have the three stylesheet

--- lesson 5/js/toggleMenu.1.js
+++ lesson 5/js/toggleMenu.1.js
@@ -1,9 +1,12 @@
 
-funtion toggleMenu() {
-   console.log("it worked");
-}
 
 
 var timeStamp = Math.floor(Date.now() / 1000);
 
 var d = new Date();
+
+function toggleMenu() {
+   document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
